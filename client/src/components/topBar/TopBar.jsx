f5e83d8028903d8853405f0a3508cbc19513dbda
Md Yusuf <mdyusuf742171@gmail.com>
2025-04-20 01:05:36 +0530

    crete header and sidebar

---
+++ client/src/components/topBar/TopBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './TopBar.css'
+import UserButton from '../userButton/UserButton'
+
+const TopBar = () => {
+  return (
+    <div className='topBar'>
+      <div className='search'>
+        <img src="/general/search.svg" alt="" />
+        <input type="text" placeholder='Search' />
+      </div>
+      <UserButton/>
+    </div>
+  )
+}
+
+
+export default TopBar
---
+++ client/src/components/userButton/UserButton.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./UserButton.css";
+
+const UserButton = () => {
+  const [open, setOpen] = useState(false);
+
+  const currentUser = true;
+
+  return currentUser ? (
+    <div className="userButton">
+      <img src="/general/noAvatar.png" alt="" />
+      <img
+        onClick={() => setOpen((prev) => !prev)}
+        src="/general/arrow.svg"
+        alt=""
+        className="arrow"
+      />
+      {open && (
+        <div className="userOptions">
+          <a href="/" className="userOption">
+            Profile
+          </a>
+          <a href="/" className="userOption">
+            Settings
+          </a>
+          <a href="/" className="userOption">
+            Logout
+          </a>
+        </div>
+      )}
+    </div>
+  ) : (
+    <a href="/" className="loginLink">
+      Login / Sign up
+    </a>
+  );
+};
+
+export default UserButton;
