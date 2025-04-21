ae331dd1522f6769c0b3361369b2384c94696941
Md Yusuf <mdyusuf742171@gmail.com>
2025-04-21 23:40:00 +0530

    create home page

--- client/src/components/galleryItem/GalleryItem.jsx
+++ client/src/components/galleryItem/GalleryItem.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import './GalleryItem.css'
+import React from "react";
+import "./GalleryItem.css";
+import { Link } from "react-router";
 
-const GalleryItem = ({item}) => {
+const GalleryItem = ({ item }) => {
   return (
-    <div className='galleryItem' style={{gridRowEnd:`span ${Math.ceil(item.height/100)}`}}>
-      <img src={item.media} alt="" />
+    <div
+      className="galleryItem"
+      style={{ gridRowEnd: `span ${Math.ceil(item.height / 100)}` }}
+    >
+      {/* <img src={item.media} alt="" /> */}
+      <img src={item.media} alt=""/>
+      <Link to={`/pin/${item._id}`} className="overlay" />
+      <button className="saveButton">Save</button>
+      <div className="overlayIcons">
+        <button>
+          <img src="/general/share.svg" alt="" />
+        </button>
+        <button>
+          <img src="/general/more.svg" alt="" />
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GalleryItem
+export default GalleryItem;
