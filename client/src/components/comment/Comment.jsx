0100209afd094b5facb49219921488d35831c43f
Md Yusuf <mdyusuf742171@gmail.com>
2025-04-23 20:50:11 +0530

    add emoji picker and comment section to Comment component

--- client/src/components/comment/Comment.jsx
+++ client/src/components/comment/Comment.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./comment.css";
+import Image from "../image/Image";
+import EmojiPicker from "emoji-picker-react";
 
 const Comment = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div>Comment</div>
-  )
-}
+    <div className="comments">
+      <div className="commentList">
+        <span className="commentCount">5 comment</span>
+        <div className="comment">
+          <Image path="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className="commentUsername">John Doe</span>
+            <p className="commentText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <span className="commentTime">1h</span>
+          </div>
+        </div>
+        <div className="comment">
+          <Image path="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className="commentUsername">John Doe</span>
+            <p className="commentText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <span className="commentTime">1h</span>
+          </div>
+        </div>
+        <div className="comment">
+          <Image path="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className="commentUsername">John Doe</span>
+            <p className="commentText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <span className="commentTime">1h</span>
+          </div>
+        </div>
+        <div className="comment">
+          <Image path="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className="commentUsername">John Doe</span>
+            <p className="commentText">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            </p>
+            <span className="commentTime">1h</span>
+          </div>
+        </div>
+      </div>
+      <form className="commentForm">
+        <input type="text" placeholder="Add a comment..." />
+        <div className="emoji">
+          <div onClick={() => setOpen((prev) => !prev)}>😊</div>
+          {open && (
+            <div className="emojiPicker">
+              <EmojiPicker />
+            </div>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Comment
+export default Comment;
