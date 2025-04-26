26fbd02e104993175e18f3e03c40af2879ff5b54
Md Yusuf <mdyusuf742171@gmail.com>
2025-04-26 11:30:24 +0530

    create auth page

--- client/src/routes/authPage/AuthPage.jsx
+++ client/src/routes/authPage/AuthPage.jsx
@@ -1,10 +1,96 @@
-import React from 'react'
-import './AuthPage.css'
+import React, { useState } from "react";
+import "./AuthPage.css";
+import Image from "../../components/image/Image";
 
 const AuthPage = () => {
-  return (
-    <div>AuthPage</div>
-  )
-}
+  const [isRegister, setIsRegister] = useState(false);
+  const [error, setError] = useState("");
 
-export default AuthPage
+  return (
+    <div className="authPage">
+      <div className="authContainer">
+        <Image path="/general/logone.png" w={40} h={40} alt="" />
+        <h1>{isRegister ? "Create an Account" : "Login to your account"}</h1>
+        {isRegister ? (
+          <form key="register">
+            <div className="formGroup">
+              <label htmlFor="username">username</label>
+              <input
+                type="username"
+                placeholder="Enter your username"
+                required
+                name="username"
+                id="username"
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="displayName">Name</label>
+              <input
+                type="displayName"
+                placeholder="Name"
+                required
+                name="displayName"
+                id="displayName"
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                required
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Enter your password"
+                required
+                name="password"
+                id="password"
+              />
+            </div>
+            <button type="submit">Register</button>
+            <p onClick={() => setIsRegister(false)}>
+              Do you have an account? <b>Login</b>
+            </p>
+            {error && <p className="error">{error}</p>}
+          </form>
+        ) : (
+          <form key="loginForm">
+            <div className="formGroup">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                required
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className="formGroup">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Enter your password"
+                required
+                name="password"
+                id="password"
+              />
+            </div>
+            <button type="submit">Login</button>
+            <p onClick={() => setIsRegister(true)}>
+              Don&apos;t have an account? <b>Register</b>
+            </p>
+            {error && <p className="error">{error}</p>}
+          </form>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AuthPage;
