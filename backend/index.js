227a92551cbdb88415644325ffbd354fd5dbdd90
Md Yusuf <mdyusuf742171@gmail.com>
2025-04-28 11:26:15 +0530

    setup express server mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
-
+import userRouter from "./routes/user.route.js"
+import pinRouter from "./routes/pin.route.js"
+import commentRouter from "./routes/comment.route.js"
+import boardRouter from "./routes/board.route.js"
+import connectDB from './utils/connectDB.js';
 const app = express()
 
-app.use("/test",(req,res) => {
-    return res.json("Hello from the backend")
-})
+app.use("/users",userRouter)
+app.use("/pins",pinRouter)
+app.use("/comment",commentRouter)
+app.use("/board",boardRouter)
+
 
 app.listen(3000,() => {
+    connectDB()
     console.log('Server is running on port 3000')
 })
